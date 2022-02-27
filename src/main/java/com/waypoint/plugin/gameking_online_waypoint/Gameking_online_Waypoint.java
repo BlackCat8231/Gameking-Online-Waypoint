124877e7a81c3ee0606672991569f190befabf42
BlackCat8231 <kej8502@gmail.com>
2022-02-27 14:51:48 +0900

    firstcommit

---
+++ src/main/java/com/waypoint/plugin/gameking_online_waypoint/Gameking_online_Waypoint.java
@@ -0,0 +1,17 @@
+package com.waypoint.plugin.gameking_online_waypoint;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Gameking_online_Waypoint extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
