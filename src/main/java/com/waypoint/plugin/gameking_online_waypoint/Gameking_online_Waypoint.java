a534e79a4a05c9c029dc38bbf4f824a08372efc0
korea-superman <permansu723@gmail.com>
2022-02-27 20:36:52 +0900

    waypoint 커맨드 만듬

--- src/main/java/com/waypoint/plugin/gameking_online_waypoint/Gameking_online_Waypoint.java
+++ src/main/java/com/waypoint/plugin/gameking_online_waypoint/Gameking_online_Waypoint.java
@@ -10,6 +10,8 @@ import org.jetbrains.annotations.NotNull;
 
 public final class Gameking_online_Waypoint extends JavaPlugin {
 
+    private final String WrongWaypointCommand = ChatColor.RED + "usage: /waypoint [get / set] <이름>";
+
     @Override
     public void onEnable() {
         // Plugin startup logic
@@ -20,8 +22,20 @@ public final class Gameking_online_Waypoint extends JavaPlugin {
         { // 내가 왔다갔다는 증거 superman!
             sender.sendMessage(ChatColor.AQUA+"웨이포인트 사용 방법");
             sender.sendMessage(ChatColor.BLUE+"/Waypoint set 이름 :웨이포인트를 생성합니다");
-                    sender.sendMessage(ChatColor.BLUE+"/Waypoint get 이름 : 생성한 웨이포인트의 좌표를 봅니다");
-
+            sender.sendMessage(ChatColor.BLUE+"/Waypoint get 이름 : 생성한 웨이포인트의 좌표를 봅니다");
+            if (args.length == 0) {
+                sender.sendMessage(WrongWaypointCommand);
+            } else if (args.length == 1) {
+                sender.sendMessage(WrongWaypointCommand);
+            } else if (args.length == 2) {
+                if (args[1].equalsIgnoreCase("get")) {
+                    sender.sendMessage("get 아직 안 만듬");
+                } else if (args[1].equalsIgnoreCase("set")) {
+                    sender.sendMessage("set 아직 안 만듬");
+                }
+            } else {
+                sender.sendMessage(WrongWaypointCommand);
+            }
         }
 
         return true;
