b62a5cc1b18afb83bf789c6adc0323cdd3d62f0c
korea-superman <permansu723@gmail.com>
2022-02-27 20:17:57 +0900

    내가 커밋함. 슈퍼맨

--- src/main/java/com/waypoint/plugin/gameking_online_waypoint/Gameking_online_Waypoint.java
+++ src/main/java/com/waypoint/plugin/gameking_online_waypoint/Gameking_online_Waypoint.java
@@ -17,7 +17,7 @@ public final class Gameking_online_Waypoint extends JavaPlugin {
     }
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args){
         if(command.getName().equalsIgnoreCase("Waypoint"))
-        {
+        { // 내가 왔다갔다는 증거 superman
             sender.sendMessage(ChatColor.AQUA+"웨이포인트 사용 방법");
             sender.sendMessage(ChatColor.BLUE+"/Waypoint set 이름 :웨이포인트를 생성합니다");
                     sender.sendMessage(ChatColor.BLUE+"/Waypoint get 이름 : 생성한 웨이포인트의 좌표를 봅니다");
