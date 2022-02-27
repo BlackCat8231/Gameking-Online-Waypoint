90c741f096c3a8c7928e53857f8de5d9123450a2
BlackCat8231 <kej8502@gmail.com>
2022-02-27 19:58:15 +0900

    commit

--- src/main/java/com/waypoint/plugin/gameking_online_waypoint/Gameking_online_Waypoint.java
+++ src/main/java/com/waypoint/plugin/gameking_online_waypoint/Gameking_online_Waypoint.java
@@ -1,6 +1,12 @@
 package com.waypoint.plugin.gameking_online_waypoint;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 public final class Gameking_online_Waypoint extends JavaPlugin {
 
@@ -8,6 +14,17 @@ public final class Gameking_online_Waypoint extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
 
+    }
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args){
+        if(command.getName().equalsIgnoreCase("Waypoint"))
+        {
+            sender.sendMessage(ChatColor.AQUA+"웨이포인트 사용 방법");
+            sender.sendMessage(ChatColor.BLUE+"/Waypoint set 이름 :웨이포인트를 생성합니다");
+                    sender.sendMessage(ChatColor.BLUE+"/Waypoint get 이름 : 생성한 웨이포인트의 좌표를 봅니다");
+
+        }
+
+        return true;
     }
 
     @Override
